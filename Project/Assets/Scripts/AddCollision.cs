7b8f115b3f99e9afc680714edff730a1c414329a
cdolbier <c.dolbier@ufl.edu>
2016-03-28 08:27:12 -0400

    got new stage. got scene set up

---
+++ Project/Assets/Scripts/AddCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake()
+    {
+        addCollider(transform);
+    }
+
+    void addCollider(Transform g)
+    {
+        if(g.GetComponent<MeshFilter>())
+            g.gameObject.AddComponent<MeshCollider>();
+
+        foreach (Transform t in g){
+            addCollider(t);
+        }
+    }
+}
