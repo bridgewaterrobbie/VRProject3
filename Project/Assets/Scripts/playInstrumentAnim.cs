2e3ad6999fd96e2d0f50d91f775f3af5b096dacd
Robbie Bridgewater <bridgewaterrobbie@gmail.com>
2016-03-30 19:03:21 -0400

    fixed up

---
+++ Project/Assets/Scripts/playInstrumentAnim.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class playInstrumentAnim : MonoBehaviour {
+
+	public GameObject theInstrument;
+
+	//blah blah blah
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!theInstrument.GetComponent<AudioSource> ().mute) {
+			theInstrument.GetComponent<Animation> ().Play ();
+		} else {
+			theInstrument.GetComponent<Animation> ().Stop();
+		}
+	}
+}
