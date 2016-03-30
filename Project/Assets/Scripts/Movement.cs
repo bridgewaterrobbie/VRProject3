b367230ac5ef854c1aa1d720aa186888109ec59d
cdolbier <c.dolbier@ufl.edu>
2016-03-30 17:41:26 -0400

    updated movement

--- Project/Assets/Scripts/Movement.cs
+++ Project/Assets/Scripts/Movement.cs
@@ -4,14 +4,68 @@ using System.Collections;
 public class Movement : MonoBehaviour {
     public float speed;
     public CardboardHead head;
+    private float holdTime;
+    private bool jumping;
+    private bool colliding;
+    private bool freeCam;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(Time.time - holdTime >= 1)
+            jumping = false;
+        colliding = true;
+    }
+    
+    void OnCollisionExit(Collision other)
+    {
+        colliding = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+            freeCam = !freeCam;
+
+        if (freeCam)
+        {
+            jumping = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        }
+        else
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        }
+
+        if(Input.GetKey(KeyCode.Space) && freeCam)
+        {
+            transform.Translate(0, 3 * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !jumping && !freeCam)
+        {
+            jumping = true;
+            holdTime = Time.time;
+        }
+        if(jumping && Time.time - holdTime < 1)
+        {
+            transform.Translate(0, 3 * Time.deltaTime, 0);
+        }
+        else
+        {
+            jumping = false;
+        }
+        if(!jumping && !colliding && !freeCam)
+        {
+            transform.Translate(0, -3 * Time.deltaTime, 0);
+        }
+
         float transForward = Input.GetAxis("Vertical");
         float transSide = Input.GetAxis("Horizontal");
 
@@ -23,7 +77,15 @@ public class Movement : MonoBehaviour {
         else
             dir = new Vector3(0, 0, 0);
 
-        transform.position += speed * transForward * head.Gaze.direction * Time.deltaTime;
+        Vector3 dir2 = head.Gaze.direction;
+        if(!freeCam)
+        {
+            dir2 = new Vector3(dir2.x, 0, dir2.z);
+            transform.position += speed * transForward * dir2 * Time.deltaTime;
+        }
+        else
+            transform.position += speed * transForward * head.Gaze.direction * Time.deltaTime;
+
         transform.position += speed * dir * Time.deltaTime;
     }
 }
